dca9af6115d9cb2bbdbb029a048a8c1058c484a1
khvilaboa <khvilaboa@esei.uvigo.es>
2017-03-05 17:22:27 +0100

    Boundary
    Objects that leave the boundary are destroyed.

---
+++ Assets/Scripts/BoundaryController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundaryController : MonoBehaviour {
+
+    void OnTriggerExit(Collider coll)
+    {
+        Destroy(coll.gameObject);
+        Debug.Log("Exit:" + coll);
+    }
+}
