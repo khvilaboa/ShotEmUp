3542b9b0d7b5cc024cf59e9bd4231cadaa17e3fb
khvilaboa <khvilaboa@esei.uvigo.es>
2017-03-05 12:23:35 +0100

    Basic starship and movement
    With options to limit the spaceship speed and the boundaries of the
    screen.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    
+
+    [Header("Movement")]
+    public float speed;
+    public float tiltAngle;
+    public float minX, maxX, minZ, maxZ;
+    private Rigidbody body;
+
+    void Start () {
+        body = GetComponent<Rigidbody>();
+	}
+
+    void FixedUpdate () {
+        // Get movements from inputs
+        float horizontalMov = Input.GetAxis("Horizontal");
+        float verticalMov = Input.GetAxis("Vertical");
+
+        // Set the new volocity
+        Vector3 movementVector = new Vector3(horizontalMov, 0, verticalMov);
+        body.velocity = movementVector * speed;
+
+        // Check that the player is inside the boundaries
+        float newX = Mathf.Clamp(body.position.x, minX, maxX);
+        float newZ = Mathf.Clamp(body.position.z, minZ, maxZ);
+        body.position = new Vector3(newX, 0, newZ);
+
+        // Tilt the player when moving
+        float currentTiltAngle = -(body.velocity.x / speed) * tiltAngle;
+        body.rotation = Quaternion.Euler(0, 0, currentTiltAngle);
+    }
+
+}
