ac99b1e9898d999de6ec8fc1f8b9f6933b57eeee
khvilaboa <khvilaboa@esei.uvigo.es>
2017-03-05 16:05:37 +0100

    Asteroids
    Basic asteroid (with rotation and colission detection to destroy
    itself).

---
+++ Assets/Scripts/AsteroidMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidMovement : MonoBehaviour {
+
+    private Rigidbody body;
+    public float speedLimit;
+
+    void Awake() {
+        body = GetComponent<Rigidbody>();
+    }
+
+	void Start () {
+        body.angularVelocity = new Vector3(Random.Range(-speedLimit, speedLimit),
+                                           Random.Range(-speedLimit, speedLimit),
+                                           Random.Range(-speedLimit, speedLimit));
+	}
+
+    void OnTriggerEnter(Collider coll) {
+        if(coll.tag != "boundary") {
+            Destroy(coll.gameObject);  // Shot
+            Destroy(gameObject);  // Asteroid
+        }
+    }
+}
