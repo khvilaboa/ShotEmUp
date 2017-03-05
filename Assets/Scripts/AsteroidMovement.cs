a7b2acc8f8fd6c2d1a3ae61d04e55e43b2471dc6
khvilaboa <khvilaboa@esei.uvigo.es>
2017-03-05 18:25:26 +0100

    Asteroid explosion
    And random size.

--- Assets/Scripts/AsteroidMovement.cs
+++ Assets/Scripts/AsteroidMovement.cs
@@ -6,19 +6,26 @@ public class AsteroidMovement : MonoBehaviour {
 
     private Rigidbody body;
     public float speedLimit;
+    public GameObject explosion;
 
     void Awake() {
         body = GetComponent<Rigidbody>();
     }
 
 	void Start () {
+        // Random rotation
         body.angularVelocity = new Vector3(Random.Range(-speedLimit, speedLimit),
                                            Random.Range(-speedLimit, speedLimit),
                                            Random.Range(-speedLimit, speedLimit));
+
+        // Random size 
+        float scale = Random.Range(4, 8);
+        transform.localScale = new Vector3(scale, scale, scale);
 	}
 
     void OnTriggerEnter(Collider coll) {
         if(coll.tag != "boundary") {
+            Instantiate(explosion, transform.position, transform.rotation);
             Destroy(coll.gameObject);  // Shot
             Destroy(gameObject);  // Asteroid
         }
