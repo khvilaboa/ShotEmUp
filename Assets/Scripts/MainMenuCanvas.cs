8a7d8a792316b82a4cb4c52186c57070cb8b2575
Iván Martínez Mateu <imm@ivanmartinezmateu.com>
2017-04-28 20:39:40 +0200

    Implemented main menu

---
+++ Assets/Scripts/MainMenuCanvas.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuCanvas : MonoBehaviour
+{
+    public Button startButton;
+    public Button optionsButton;
+    public Button exitButton;
+
+    void Start()
+    {
+        startButton.GetComponent<Button>().onClick.AddListener(TaskOnStartButtonClick);
+        optionsButton.GetComponent<Button>().onClick.AddListener(TaskOnOptionsButtonClick);
+        exitButton.GetComponent<Button>().onClick.AddListener(TaskOnExitButtonClick);
+    }
+
+    void TaskOnStartButtonClick()
+    {
+        SceneManager.LoadScene("gameScene", LoadSceneMode.Single);
+    }
+
+    void TaskOnOptionsButtonClick()
+    {
+        Debug.Log("You have option clicked the button!");
+    }
+
+    void TaskOnExitButtonClick()
+    {
+        Application.Quit();
+    }
+}
