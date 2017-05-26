e33228efe8f5420e5f2b139fb76216a68e98a7aa
Iván Martínez Mateu <imm@ivanmartinezmateu.com>
2017-05-26 20:11:19 +0200

    Added Game Options storage class

---
+++ Assets/Scripts/GameOptions.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class GameOptions : MonoBehaviour {
+    static public bool isVolumeEnabled;
+    static public bool areSoundEffectsEnabled;
+    public enum InputMode
+    {
+        OnlyKeyboard,
+        OnlyGesture,
+        KeyboardAndGesture
+    };
+    static public InputMode inputModeSelected;
+
+    public bool IsVolumeEnabled
+    {
+        get
+        {
+            return isVolumeEnabled;
+        }
+
+        set
+        {
+            isVolumeEnabled = value;
+        }
+    }
+
+    public bool AreSoundEffectsEnabled
+    {
+        get
+        {
+            return areSoundEffectsEnabled;
+        }
+
+        set
+        {
+            areSoundEffectsEnabled = value;
+        }
+    }
+
+    public InputMode InputModeSelected
+    {
+        get
+        {
+            return inputModeSelected;
+        }
+
+        set
+        {
+            inputModeSelected = value;
+        }
+    }
+}
