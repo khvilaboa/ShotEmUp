f72607e019133d634c0a106032acfc4662f9a69b
Iván Martínez Mateu <imm@ivanmartinezmateu.com>
2017-06-01 18:20:59 +0200

    Add FPS limiter

---
+++ Assets/Scripts/GameResolution.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameResolution : MonoBehaviour {
+    public int fpsLimit = 45;
+
+    void Awake()
+    {
+        Application.targetFrameRate = fpsLimit;
+        #if UNITY_EDITOR
+            QualitySettings.vSyncCount = 0;  // VSync must be disabled
+            Application.targetFrameRate = fpsLimit;
+        #endif
+        }
+}
